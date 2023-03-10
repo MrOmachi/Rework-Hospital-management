5161e4f7c18360b5db9d727285999465c29dfeae
Abraham <brhmdeen@gmail.com>
2023-03-10 13:54:30 +0100

    footer added to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 
 import React, { useState, useEffect } from "react";
+import Footer from "../components/Footer";
 import HeadBar from "../components/Headbar";
 function Home() {     
         return (
             <>
                         <HeadBar title="Home"/>
+
+                        <Footer/>
             </>
         );
 }

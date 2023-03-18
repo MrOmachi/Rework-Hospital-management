5952158d331aed88991bbca384a4c6bc157e00ff
monsaw <benjamineffiong124@yahoo.com>
2023-03-18 21:07:58 +0100

    Additional touches for big screens

--- cleva-banking/src/components/Convert.tsx
+++ cleva-banking/src/components/Convert.tsx
@@ -11,8 +11,8 @@ const Convert = (props: IConvert) => {
        <img src={convert} />
      </div>
      <div>
-      <h1 className="flex items-center">Convert USD <img src={right} className='w-[15.66px] h-[7.83px]'/> NGN and NGN <img src={right} className='w-[15.66px] h-[7.83px]'/>USD</h1>
-      <p className='text-[#000000] font-[500] text-[19.5716px] leading-[24px]'>Convert currencies instantly, for free, at the best rate, and with just one click</p>
+      <h1 className="flex items-center text-[35.2289px] leading-[43px] font-[600]">Convert USD <img src={right} className='w-[15.66px] h-[7.83px]'/> NGN and NGN <img src={right} className='w-[15.66px] h-[7.83px]'/>USD</h1>
+      <p className='text-[#000000] font-[500] text-[19.5716px] leading-[24px] w-[421.77px]'>Convert currencies instantly, for free, at the best rate, and with just one click</p>
      </div>
     </div>
   )
--- cleva-banking/src/components/StopStressing.tsx
+++ cleva-banking/src/components/StopStressing.tsx
@@ -7,20 +7,20 @@ interface IStopStressing { }
 
 const StopStressing = (props: IStopStressing) => {
  return (
-  <div className="bg-[#FFF8EE] flex flex-col justify-center mt-[63.61px] items-center mb-[76.33px]">
+  <div className="bg-[#FFF8EE] flex flex-col justify-center mt-[63.61px] items-center mb-[76.33px] mx-[82.2px] px-[130.15px] rounded-[45.9933px]">
    <h1 className="text-[#000000] text-[33.2718px] leading-[41px] font-[600] mt-[45.01px]">Stop stressing about</h1>
 
-   <div className='flex mb-[96.1px] justify-between'>
-    <div className="flex flex-col">
-     <img src={one} className="w-[97.86px]" />
+   <div className='flex mb-[96.1px] justify-between mt-[49.03px]'>
+    <div className="flex flex-col items-center">
+     <img src={one} className="w-[97.86px] mb-[19.57px]" />
      <h1 className='text-[33.2718px] leading-[41px] font-[600]'>Cross-border payments</h1>
     </div>
-    <div className="flex flex-col">
-     <img src={two} className="w-[97.86px]" />
+    <div className="flex flex-col items-center">
+     <img src={two} className="w-[97.86px] mb-[19.57px]" />
      <h1 className='text-[33.2718px] leading-[41px] font-[600]'>Tracking employee and company spend</h1>
     </div>
-    <div className="flex flex-col">
-     <img src={three} className="w-[97.86px]" />
+    <div className="flex flex-col items-center">
+     <img src={three} className="w-[97.86px] mb-[19.57px]" />
      <h1 className='text-[33.2718px] leading-[41px] font-[600]'>Paying your vendors on-time </h1>
     </div>
    </div>

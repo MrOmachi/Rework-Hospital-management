4af1bf55985e2b439523a692ead5470d725986b2
Sifon Isaac <syfonisaac@gmail.com>
2023-06-20 13:02:11 +0100

    push update

--- cleva-frontend/src/App.tsx
+++ cleva-frontend/src/App.tsx
@@ -7,12 +7,9 @@ import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { getReturningUser, removeAuthTokens } from "./login";
 import {toast} from "react-toastify"
 import { setUser } from "./features/Accounts/AccountSlice";
-import Spinner from "./components/PopUps/Spinner";
 
 function App() {
   const user = useAppSelector((state) => state.account.user);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true); // Added loading state
   const dispatch = useAppDispatch();
 
   // localStorage.setItem("KycIdentifier","kyc-lizqlf41-2pqqtf");
@@ -56,35 +53,12 @@ function App() {
           removeAuthTokens();
           toast.error("Session expired, please login again");
         })
-        .finally(() => {
-          setIsLoading(false); 
-        });
+       
     }, [dispatch]);
-  
-    useEffect(() => {
-      const authToken = localStorage.getItem('accessToken');
-      // console.log(authToken)
-      if (authToken) {
-        // Set the user as authenticated
-        setIsAuthenticated(true);
-      }
-    }, []);
-  
-    // if (isLoading) {
-    //   return <Spinner/>
-    // }
   
     return (
       <>
           <RouterProvider router={routes(user)} />
-
-        {/* {isAuthenticated ? (
-          <RouterProvider router={routes(user)} />
-        ) 
-        
-        : (
-          <p>Please login to access the app.</p>
-        )} */}
       </>
     );
   }
